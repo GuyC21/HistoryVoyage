79a8f600771b4ff3c57cd81064b6f699382de0ec
GuyC21 <e4guycohen@outlook.com>
2026-06-29 15:09:08 +0300

    added english translations caching

--- backend/heritage/serializers.py
+++ backend/heritage/serializers.py
@@ -1,8 +1,12 @@
+from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import HistoricalSite
 
 class HistoricalSiteSerializer(GeoFeatureModelSerializer):
+    englishName = serializers.CharField(source='english_name', read_only=True, allow_null=True)
+    englishDescription = serializers.CharField(source='english_description', read_only=True, allow_null=True)
+
     class Meta:
         model = HistoricalSite
         geo_field = 'location'
-        fields = ('id', 'name', 'site_type', 'country', 'description', 'wikidata')
+        fields = ('id', 'name', 'site_type', 'country', 'description', 'wikidata', 'englishName', 'englishDescription')
